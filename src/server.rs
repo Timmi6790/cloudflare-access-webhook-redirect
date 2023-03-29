ac8b3cac86c8bd4857abeb320a2be3fb8aab3fdf
Tim <13711365+Timmi6790@users.noreply.github.com>
2023-03-29 14:05:08 +0200

    Add derive_new macro

--- src/server.rs
+++ src/server.rs
@@ -1,19 +1,16 @@
 use crate::data::WebHookData;
-use actix_web::{web, App, HttpServer};
-
 use crate::routes::{health_check, redirect};
 use crate::Result;
+use actix_web::{web, App, HttpServer};
+use derive_new::new;
 
+#[derive(new)]
 pub struct Server {
     host: String,
     port: u16,
 }
 
 impl Server {
-    pub fn new(host: String, port: u16) -> Self {
-        Self { host, port }
-    }
-
     pub async fn run_until_stopped(&self, web_hook_data: WebHookData) -> Result<()> {
         info!(
             "Starting server on {}:{} with allowed paths {:#?}",
